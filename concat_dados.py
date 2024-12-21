6ec6a19b8e99f4d8beb485add1e2347145df19fa
Daniele Karina dos Santos <danimistron@gmail.com>
2024-12-21 09:41:04 +0000

    inserção códigos python

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+#Vamos receber 2 dados diferentes do usuário e concatena-los.
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenadas são: ", info_concatenada)
